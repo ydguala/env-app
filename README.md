b044faaea4d705f4c1ebce552380b128bfeb4e73
ydguala <ydguala@example.com>
2023-04-06 09:15:07 +0200

    env-api v2

---
+++ README.md
@@ -0,0 +1,37 @@
+# env-api
+## Intro
+This is a python Flask app that generates a web app that returns a custom message.
+
+It is also used for retrieving the hostname where the app is running and debugging some env vars from the host too.
+
+## How does it work?
+
+This python app requires 3 args.
+
+```
+python3 api-server.py "my_app" "USER" "Good Morning"
+```
+
+- ARG 1: app name
+- ARG 2: host environment variable name
+- ARG 3: custom message
+
+It will use the vars to present a message like: app_name, var2 and custom message to print a message like :
+```
+Hello, World! This is app my_app! I have a var called USER with value ydguala. And I have a message for you: Good Morning
+```
+
+## Endpoints
+It expose some endpoints that only allow method `GET` for the web app to retrieve some values.
+
+|Endpoint|Method|Description|
+|---|---|---|
+|/| GET | Returns the custom message |
+|/status | GET | Returns a json with the status, name of the app and the time |
+|/${app_name} | GET | Returns a json with the key 'app_name' and value of ARG 1 |
+|${app_name}/api | GET | Returns a json with the key 'api' and value of ARG 1 |
+|/${app_name}/api/message | GET | Returns a json with the key 'message' and value of the message passed as ARG 3 |
+|/\${app_name}/api/${var2}| GET | Returns a json with the key passed as ARG 2 and value of the host environment variable of this var. If var not found returns 'UNKNOWN' |
+|/\${app_name}/api/hostname| GET | Returns a json with the key 'HOSTNAME' and value of the host that is running the Flask app |
+
+
--- api-server.py
+++ api-server.py
@@ -9,52 +9,117 @@ from datetime import datetime
 from flask import *
 
 
+
+
+
+# FUNCTIONS
+
+# This function will get the first 3 args and set them in order to app_name,var2 and message
+def set_args_as_vars():
+    help_message = """
+    This app will take first 3 args as env var names that will be used as: app_name, var2 and message to print a message like : '<p>Hello, World {app_name}! {var2} = {var2_value}, message: {message}</p>'
+    """
+
+    if len(sys.argv) < 3:
+        raise Exception('-------- ERROR - need 3 args. {}'.format(help_message))
+    else:
+        global app_name
+        global var2
+        global message
+
+        app_name = str(sys.argv[1])
+        var2 = str(sys.argv[2])
+        message = str(sys.argv[3])
+
+
+def get_env_var_value(var):
+    try:
+        env_var = os.environ[var]
+    except KeyError:
+        print("{} not found".format(var))
+        env_var = "UNKNOWN"
+
+    if var == 'HOSTNAME':
+            env_var = os.uname()[1]
+
+
+    return env_var
+
 # GLOBAL VARS
 server_port         = 8080
 server_host         = "0.0.0.0"
 api_server          = Flask('api-server')
 
+# needed to set next vars
+set_args_as_vars()
 
-# FUNCTIONS
+app_name_value = get_env_var_value(app_name)
+var2_value = get_env_var_value(var2)
+
+
+# FLASK FUNCTIONS, needed to be defined after params
 
 @api_server.route('/', methods=['GET'])
-def  home():
+def  root():
 
-    response = {'status' : 'ok', 'message' : 'home', 'time' : datetime.now().strftime("%Y-%m-%dT%H:%M:%S") }
+
+    return '<p>Hello, World! This is the app: {}! I have a var called {} with value {}. And I have a message for you: {}</p>'.format(app_name, var2,var2_value, message)
+
+
+@api_server.route('/status', methods=['GET'])
+def  status():
+
+    response = {'app' : app_name_value , 'status' : 'ok', 'time' : datetime.now().strftime("%Y-%m-%dT%H:%M:%S") }
     json_response = json.dumps(response)
 
     return json_response
 
-@api_server.route('/env', methods=['GET'])
-def  env():
+@api_server.route('/{}'.format(app_name_value), methods=['GET'], strict_slashes=False)
+def app():
+    response = { app_name : app_name_value }
 
-    response = {'env' : dict(os.environ)  }
     json_response = json.dumps(response)
 
     return json_response
 
-@api_server.route('/hostname', methods=['GET'])
-def  hostname():
+@api_server.route('/{}/api'.format(app_name_value), methods=['GET'], strict_slashes=False)
+def api():
+    response = { 'api' : app_name_value  }
 
-    # get hostname
+    json_response = json.dumps(response)
+
+    return json_response
+
+@api_server.route('/{}/api/message'.format(app_name_value), methods=['GET'], strict_slashes=False)
+def api_message():
+    response = { 'message' : message  }
+
+    json_response = json.dumps(response)
+
+    return json_response
+
+@api_server.route('/{}/api/{}'.format(app_name_value, var2), methods=['GET'], strict_slashes=False)
+def api_custom_var():
+
+    response = { var2 : var2_value }
+
+    json_response = json.dumps(response)
+
+    return json_response
+
+
+@api_server.route('/{}/api/hostname'.format(app_name_value), methods=['GET'], strict_slashes=False)
+def api_hostname():
+
     try:
         hostname = os.uname()[1]
-    except KeyError as e:
-        print("hostname not found {}".format(e))
+    except KeyError:
+        print("{} not found".format(var))
         hostname = "UNKNOWN"
 
-    # get namespace
-    try:
-        ns = os.environ['NAMESPACE']
-    except KeyError as e:
-        print("namespace not found {}".format(e))
-        ns = "UNKNOWN"
-
-    response = { 'HOSTNAME' : hostname , 'NAMESPACE' : ns }
+    response = { 'HOSTNAME' : hostname }
 
     json_response = json.dumps(response)
-
-    print("\n ==========> /hostname : {} , namespace : {}\n".format(hostname,ns))
 
     return json_response
 
@@ -62,8 +127,9 @@ def  hostname():
 
 def main():
 
-    api_server.run(host=server_host,port=server_port)
+    print("app_name arg {} - value {}\nvar2 arg {} - value {}\nmessage arg: {}".format(app_name, app_name_value, var2, var2_value, message))
 
+    api_server.run(host=server_host,port=server_port)
 
 # EXECUTION
 if __name__ == "__main__":
